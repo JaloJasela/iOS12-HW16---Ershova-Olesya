65e4930f9d9d38cddea2129dfd76605f406cdbf7
JaloJasela <jasela@yandex.ru>
2024-02-19 01:50:44 +0300

    Initial Commit

---
+++ iOS12-HW16 - Ershova Olesya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS12-HW16 - Ershova Olesya
+//
+//  Created by IMac on 19.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
